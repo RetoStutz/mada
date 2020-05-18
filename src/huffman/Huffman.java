f1ed3e35edafb937ad00f403e957512eb8675b16
Reto Stutz <reto.stutz@students.fhnw.ch>
2020-05-18 22:28:42 +0200

    update huffman

--- src/huffman/Huffman.java
+++ src/huffman/Huffman.java
@@ -1,8 +1,13 @@
 package huffman;
 
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -11,16 +16,48 @@ import java.util.stream.Collectors;
 
 public class Huffman {
 
-    //Filename
-    private static final String FILE_NAME = "C:/Users/RetoStutz/Documents/FHNW/mada_2iCbb/Übungen/Programmieraufgabe-Huffman/TextdateiAscII.txt";
+    //Filename read
+    private static final String READ_FILE_FROM = "C:/Users/RetoStutz/Documents/FHNW/mada_2iCbb/Übungen/Programmieraufgabe-Huffman/TextdateiAscII.txt";
+    //filename write
+    private static final String WRITE_FILE_TO = "C:/Users/RetoStutz/Documents/FHNW/mada_2iCbb/Übungen/Programmieraufgabe-Huffman/dec_tab.txt";
 
     String inputStream;
     Map<Character, Leaf> dataMap = new HashMap<>();
 
 
-    public Huffman () {
+    public Huffman() {
+        //read ascii file
+        readFromFile();
+        sortData();
+
+        //creat hufmann code
+        while (dataMap.size() > 1) {
+            createTree();
+            sortData();
+        }
+
+        //write to file
+        Leaf main = dataMap.entrySet()
+                .stream()
+                .map(Map.Entry::getValue)
+                .findFirst().orElse(null);
+        printCode(main, "");
+
+    }
+
+    public void sortData() {
+        dataMap = dataMap.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+    }
+
+    public void readFromFile() {
         try {
-            File myObj = new File(FILE_NAME);
+            File myObj = new File(READ_FILE_FROM);
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
                 inputStream = myReader.nextLine();
@@ -33,42 +70,17 @@ public class Huffman {
 
         String[] dataArray = inputStream.split(" ");
 
-        for (String s: dataArray){
-            Character c = (char)Integer.parseInt(s);
+        for (String s : dataArray) {
+            Character c = (char) Integer.parseInt(s);
             if (dataMap.containsKey(c)) {
                 dataMap.get(c).incrementFrequency();
-            }
-            else {
+            } else {
                 dataMap.put(c, new Leaf(c));
             }
         }
-
-        sortData();
-
-        while (dataMap.size() > 1) {
-            createTree();
-            sortData();
-        }
-
-        Leaf main = dataMap.entrySet()
-                .stream()
-                .map(Map.Entry::getValue)
-                .findFirst().orElse(null);
-        printCode (main, "");
-
     }
 
-    public void sortData(){
-        dataMap = dataMap.entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByValue())
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        Map.Entry::getValue,
-                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
-    }
-
-    public void createTree(){
+    public void createTree() {
 
 
         Leaf leftLeaf;
@@ -89,11 +101,11 @@ public class Huffman {
 
         dataMap.remove(rightLeaf.getCharacter());
 
-        dataMap.put(leftLeaf.getCharacter(), new Leaf(leftLeaf, rightLeaf, leftLeaf.getCharacter(), (leftLeaf.getFrequency()+rightLeaf.getFrequency())));
+        dataMap.put(leftLeaf.getCharacter(), new Leaf(leftLeaf, rightLeaf, leftLeaf.getCharacter(), (leftLeaf.getFrequency() + rightLeaf.getFrequency())));
 
     }
 
-    public void printCode(Leaf leaf,String s){
+    public void printCode(Leaf leaf, String s) {
         if (leaf.getLeftLeaf()
                 == null
                 && leaf.getRightLeaf()
@@ -102,11 +114,12 @@ public class Huffman {
 
 
             System.out.println(leaf.getCharacter() + ":" + s);
+            writeToFile(leaf.getCharacter() + ":" + s);
 
             return;
         }
 
-        if (leaf.getLeftLeaf() != null){
+        if (leaf.getLeftLeaf() != null) {
             printCode(leaf.getLeftLeaf(), s + "0");
         }
         if (leaf.getRightLeaf() != null) {
@@ -114,4 +127,17 @@ public class Huffman {
         }
     }
 
+    public void writeToFile(String s) {
+        Path path = Paths.get(WRITE_FILE_TO);
+        try {
+            if (!Files.exists(path)) {
+                Files.createFile(path);
+            }
+            BufferedWriter writer = Files.newBufferedWriter(path);
+            writer.append(s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
